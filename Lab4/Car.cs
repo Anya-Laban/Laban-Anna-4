9060365a6f042620335549efa6b38200a2e8af49
Anya-Laban <laban.anya.a@gmail.com>
2022-06-12 15:41:41 +0300

    Laban-Anna-4

---
+++ Lab4/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Car : Vantazhoperevisnyk
+    {
+        public Car(double weight_cargo)
+        {
+            if (weight_cargo <= Max_Weight_cargo)
+            {
+                Weight_cargo = weight_cargo;//тони
+            }
+            else Weight_cargo = Max_Weight_cargo;
+        }
+        public override int Max_Weight_cargo { get; } = 15;//ton
+        public override double Distance_of_transportation { get; protected set; }
+        public override double Speed { get; } = 80;//km/god
+        public override double fuel_price { get; } = 50;
+        public override double fuel_ponsumption { get; } = 20;
+        public override List<City> Cities { get; } = new List<City>() { new City("Київ", "Харків", 479), new City("Київ", "Львів", 541), new City("Львів", "Івано-Франківськ", 134), new City("Запоріжжя", "Харків", 320) };
+
+        public override void Choose_Distance(int num, ref bool corectNum)
+        {
+
+            if (num - 1 < Cities.Count)
+            {
+                for (int i = 0; i < Cities.Count; i++)
+                {
+                    if (num - 1 == i)
+                    {
+                        Distance_of_transportation = Cities[i].Distance;
+                        corectNum = false;
+                    }
+                }
+            }
+            else corectNum = true;
+        }
+        public override void ShowCities()
+        {
+            Console.WriteLine("Мiста, в якi здiйснюється перевезення автомобілем: ");
+            base.ShowCities();
+        }
+
+
+    }
+}
---
+++ Lab4/Vantazhoperevisnyk.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    abstract class Vantazhoperevisnyk
+    {
+        public abstract int Max_Weight_cargo { get; }//максимальна допустима масавантажу
+        public  double Weight_cargo { get; protected set; }//маса вантажу для перевезення
+       
+        public abstract double Distance_of_transportation { get;protected set; } // відстань в кілометрах
+        public abstract double Speed { get; }//середня швидкість транспорту який перевозить
+     
+        public abstract List<City> Cities  { get; }// міста, між якими можна здійснити доставку
+
+        public abstract double fuel_price { get; }//ціна за 1 літр палива
+        public abstract double fuel_ponsumption { get; }//розхід палива в літрах на 100км
+        public double Amount_of_fuel { get; private set; }//витрачене паливо
+        public double price { get; private set; }//ціна за витрачене паливо
+       
+        public double time_of_transportation { get; private set; }//приблизний час, що витрачений на дорогу
+
+        public abstract void Choose_Distance(int num, ref bool corectNum);
+
+        public virtual void ShowCities()
+        {
+            for (int i = 0; i < Cities.Count; i++)
+            {
+                Console.WriteLine(i + 1 + ") " + Cities[i].City_1 + " - " + Cities[i].City_2 + " -> " + Cities[i].Distance + " км.");
+            }
+        }
+        public double AmountOfFuel()
+        {
+            Amount_of_fuel = Distance_of_transportation / 100 * fuel_ponsumption;
+            return Amount_of_fuel;
+        }
+        public double PriceForFuel()
+        {
+            AmountOfFuel();
+            price = Amount_of_fuel * fuel_price;
+            return price;
+        }
+        public double Time_of_transportation()
+        {
+            time_of_transportation = Distance_of_transportation / Speed;
+            return time_of_transportation;
+        }
+    }
+}
